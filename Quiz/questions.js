511b7f9c1ff11d0141e693d13ef9c794bcf88aa3
Oumaima-boujlaib <159542827+Oumaima-boujlaib@users.noreply.github.com>
2024-06-18 13:35:53 +0100

    Add files via upload

---
+++ Quiz/questions.js
@@ -0,0 +1,74 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Que signifie HTML ?",
+        answer: "C. Hyper Text Markup Language",
+        options:[
+           "A. Hyper Type Multi Language",
+           "B. Hyper Text Multiple Language",
+           "C. Hyper Text Markup Language",
+           "D. Home Text Multi Language"
+
+        ]
+    },
+     {
+        numb: 2,
+        question: "Quel attribut permet d'indiquer la destination d'un lien ?",
+        answer: "A. href",
+        options:[
+            "A. href",
+            "B. class",
+            "C. a",
+            "D. data"
+        ]
+    },
+      {
+        numb: 4,
+        question: "À quoi servent les spécifications ARIA, liées à HTML ?",
+        answer: "D. à rendre le contenu et les applications web accessibles",
+        options:[
+            "A. à gérer les familles de polices sur la page web ",
+            "B. à construire des pages web audibles et musicales ",
+            "C. à aider à l'internationalisation du web  ",
+            "D. à rendre le contenu et les applications web accessibles",
+        ]
+    }, 
+     {
+        numb: 4,
+        question: "Qu'est ce qu'un navigateur web ?",
+        answer: "D. Un logiciel capable d'interpréter et d'afficher du code HTML",
+        options:[
+            "A. Un outil permettant d'accéder à Internet",
+            "B. Un réseau social globa",
+            "C. La toute première application mondiale",
+            "D. Un logiciel capable d'interpréter et d'afficher du code HTML",
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options:[
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Code Style Sheet",
+
+        ]
+    }, 
+    {
+        numb: 6,
+        question: "Dans un fichier JavaScript externe (.js), il faut :",
+        answer: "D. Aucune des réponses précédentes",
+        options:[
+            "A.  entourer le code avec les balises < script > et < /script >. ",
+            "B.  préciser l'encodage du fichier avec la règle @charset",
+            "C. échapper les caractères spéciaux (X)HTML",
+            "D. Aucune des réponses précédentes",
+
+        ]
+    }, 
+    
+
+]
